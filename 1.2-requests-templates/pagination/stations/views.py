4becb9eea5c598490d0da45a096f7513297fe5c7
Alexey Yakovlev <Yakobro@mail.ru>
2021-10-10 00:46:15 +0300

    Comlited HW_#1.2.2(Paginator)

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+from django.conf import settings
+
+import csv
+
+with open(settings.BUS_STATION_CSV, encoding='utf-8', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    bas_book = []
+    for row in reader:
+        bas_book.append(row)
 
 
 def index(request):
@@ -7,11 +17,17 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    page = int(request.GET.get('page', 1))
+    pagi = Paginator(bas_book, 10)
+
+    if page < 1:
+        page = 1
+    if page > pagi.num_pages:
+        page = pagi.num_pages
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': pagi.page(page).object_list,
+        'page': pagi.page(page),
     }
+
     return render(request, 'stations/index.html', context)
