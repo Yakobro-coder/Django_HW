924bb9d2a8c53186bc4b4dad3a8a341d0f0e42be
Alexey Yakovlev <Yakobro@mail.ru>
2021-10-09 21:35:24 +0300

    Comlited HW_#1.2.1

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponse
 
 DATA = {
     'omlet': {
@@ -16,8 +17,57 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+    'салат': {
+        'картофель, гр.': 100,
+        'морковь, гр.': 50,
+        'огурцы, гр.':50,
+        'горошек, гр.': 30,
+        'майонез, мл.': 70,
+    },
+    'пицца': {
+        'сыр, гр.': 50,
+        'томаты,гр.': 50,
+        'тесто, гр.': 100,
+        'бекон, гр.': 30,
+        'колбаса, гр.': 30,
+        'грибы, гр.': 20,
+    },
+    'фруктовый десерт': {
+        'хурма, гр.': 60,
+        'киви,гр.': 60,
+        'творог, гр.': 60,
+        'сахар, гр.': 10,
+        'мед, мл.': 50,
+    }
 }
+
+
+def home_view(request):
+    template_name = 'calculator/home.html'
+    recipe_list = [bludo for bludo in DATA]
+    context = {
+        'recipes': recipe_list
+    }
+    return render(request, template_name, context)
+
+
+def recipes_view(request, recipe):
+    template_name = 'calculator/index.html'
+    servings = request.GET.get('servings', None)
+
+    if servings is not None:
+        servings = int(servings)
+        serv_bludo = {ingrid: value*servings for ingrid, value in DATA.get(recipe).items()}
+        print(serv_bludo)
+        context = {
+            'recipe': serv_bludo
+        }
+    else:
+        context = {
+            'recipe': DATA.get(recipe)
+        }
+    return render(request, template_name, context)
+
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import home_view, recipes_view
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('', home_view, name='home'),
+    path('<recipe>/', recipes_view, name='recipes'),
+
 ]
