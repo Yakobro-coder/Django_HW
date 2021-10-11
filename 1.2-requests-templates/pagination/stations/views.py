a4233b2523349f792ef0d6168c8fd6f4e29ccd75
Alexey Yakovlev <Yakobro@mail.ru>
2021-10-11 21:00:33 +0300

    correction code in HW_#1.2.2

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -5,18 +5,18 @@ from django.conf import settings
 
 import csv
 
-with open(settings.BUS_STATION_CSV, encoding='utf-8', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    bas_book = []
-    for row in reader:
-        bas_book.append(row)
-
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
+    with open(settings.BUS_STATION_CSV, encoding='utf-8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        bas_book = []
+        for row in reader:
+            bas_book.append(row)
+            
     page = int(request.GET.get('page', 1))
     pagi = Paginator(bas_book, 10)
 
